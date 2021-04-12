9355beef60c6a45b40751873880e04bd8effca85
ZWubs <zwinters00@gmail.com>
2021-04-12 08:09:09 -0400

    Converted spaces to tabs

--- main.cpp
+++ main.cpp
@@ -21,119 +21,119 @@ float fSpeed = 0.005f;			// Walking Speed
 
 int main() {
 
-    sf::RenderWindow window(sf::VideoMode( nScreenWidth, nScreenHeight), "Ray Caster Engine");
+	sf::RenderWindow window(sf::VideoMode( nScreenWidth, nScreenHeight), "Ray Caster Engine");
 
 	sf::Image buffer;
-    buffer.create(nScreenWidth, nScreenHeight, sf::Color(0, 0, 0));
+	buffer.create(nScreenWidth, nScreenHeight, sf::Color(0, 0, 0));
 
-    sf::Texture texture;
-    texture.loadFromImage(buffer);
+	sf::Texture texture;
+	texture.loadFromImage(buffer);
 
 	sf::Sprite bufferSprite;
-    bufferSprite.setTexture(texture);
+	bufferSprite.setTexture(texture);
 
-    wstring map;
-    map += L"################";
-  	map += L"#.#............#";
-  	map += L"###............#";
-  	map += L"#..............#";
-  	map += L"#..............#";
-  	map += L"#..............#";
-  	map += L"#..............#";
-  	map += L"#..............#";
-  	map += L"#..............#";
-  	map += L"#..............#";
-  	map += L"#....######....#";
-  	map += L"#....#....#....#";
-  	map += L"#....#....#....#";
-  	map += L"#....#....#....#";
-  	map += L"#....#....#....#";
-  	map += L"################";
+	wstring map;
+	map += L"################";
+	map += L"#.#............#";
+	map += L"###............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#....##..##....#";
+	map += L"#....#....#....#";
+	map += L"#....#....#....#";
+	map += L"#....#....#....#";
+	map += L"#....#....#....#";
+	map += L"################";
 
-    while (window.isOpen()) {
+	while (window.isOpen()) {
 
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            fPlayerA -= fSpeed * 0.75f;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            fPlayerA += fSpeed * 0.75f;
-        }
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+			fPlayerA -= fSpeed * 0.75f;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+			fPlayerA += fSpeed * 0.75f;
+		}
 
-        if( sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            int sign = 1;
-            if( sf::Keyboard::isKeyPressed(sf::Keyboard::S) == true ) { sign = -1; }
-            fPlayerX += sinf(fPlayerA) * fSpeed * sign;
+		if( sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+			int sign = 1;
+			if( sf::Keyboard::isKeyPressed(sf::Keyboard::S) == true ) { sign = -1; }
+			fPlayerX += sinf(fPlayerA) * fSpeed * sign;
 			fPlayerY += cosf(fPlayerA) * fSpeed * sign;
 			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#') {
 				fPlayerX -= sinf(fPlayerA) * fSpeed * sign;
 				fPlayerY -= cosf(fPlayerA) * fSpeed * sign;
 			}
-        }
+		}
 
 
-        window.clear();
+		window.clear();
 
-        for (int x = 0; x < nScreenWidth; x++) {
-            float fRayAngle = (fPlayerA - fFOV/2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+		for (int x = 0; x < nScreenWidth; x++) {
+			float fRayAngle = (fPlayerA - fFOV/2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
 
-            float fStepSize = 0.1f;
-            float fDistanceToWall = 0.0f;
+			float fStepSize = 0.1f;
+			float fDistanceToWall = 0.0f;
 
-            bool bHitWall = false;
-            bool bBoundary = false;
+			bool bHitWall = false;
+			bool bBoundary = false;
 
-            float fEyeX = sinf(fRayAngle);
-            float fEyeY = cosf(fRayAngle);
+			float fEyeX = sinf(fRayAngle);
+			float fEyeY = cosf(fRayAngle);
 
-            while (!bHitWall && fDistanceToWall < fDepth) {
-                fDistanceToWall += fStepSize;
-                int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
-                int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+			while (!bHitWall && fDistanceToWall < fDepth) {
+				fDistanceToWall += fStepSize;
+				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
 
-                if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
-                    bHitWall = true;
-                    fDistanceToWall = fDepth;
-                }
-                else {
-                    if (map.c_str()[nTestX * nMapWidth + nTestY] == '#') {
-                        bHitWall = true;
-                    }
-                }
-            }
+				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
+					bHitWall = true;
+					fDistanceToWall = fDepth;
+				}
+				else {
+					if (map.c_str()[nTestX * nMapWidth + nTestY] == '#') {
+						bHitWall = true;
+					}
+				}
+			}
 
-            // Calculate distance to ceiling and floor
-            int nCeiling = (float)(nScreenHeight/2.0) - nScreenHeight / ((float)fDistanceToWall);
-            int nFloor = nScreenHeight - nCeiling;
+			// Calculate distance to ceiling and floor
+			int nCeiling = (float)(nScreenHeight/2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;
 
-            sf::Color color = sf::Color(0,0,0);
+			sf::Color color = sf::Color(0,0,0);
 
-            for( int y = 0; y < nScreenHeight; y++ ) {
-                if( y < nCeiling ) {
-                    color = sf::Color(0,0,0);
-                }
-                else if( y > nFloor ) {
-                    color = sf::Color(0,0,0);
-                }
-                else {
-                    int val = 255 - 256* fDistanceToWall/fDepth;
-                    color = sf::Color( val, val, val );
-                }
-                buffer.setPixel( x, y, color);
-            }
+			for( int y = 0; y < nScreenHeight; y++ ) {
+				if( y < nCeiling ) {
+					color = sf::Color(0,0,0);
+				}
+				else if( y > nFloor ) {
+					color = sf::Color(0,0,0);
+				}
+				else {
+					int val = 255 - 256* fDistanceToWall/fDepth;
+					color = sf::Color( val, val, val );
+				}
+				buffer.setPixel( x, y, color);
+			}
 
-        }
+		}
 
-        texture.update( buffer );
+		texture.update( buffer );
 
-        window.draw( bufferSprite );
-        window.display();
-    }
+		window.draw( bufferSprite );
+		window.display();
+	}
 
-    return 0;
+	return 0;
 }
