a6859a323140b8d8962bab03c24929904fe37e28
charliemikels <charliemikels@gmail.com>
2021-05-07 15:41:42 -0400

    initial ajustments were made

--- classes/entity.cpp
+++ classes/entity.cpp
@@ -5,16 +5,25 @@ using namespace sf;
 class Entity {
 
 private:
-    float fHealth;
+    int iHealth;
     Vector2f vPosition;
+	// Sprite sSprite;
+	// float fFacing;		// Redundant since Sprite also tracks facing?
+	// int iSpriteState;	// Redundant since Sprite also tracks state?
+
+	sf::Texture texture		// Placeholder for when sprites is functional
 
 public:
     Entity()
-    void Update( float fAlpha );
+    void Update( float fDelta );
 
 };
 
 // Constructor
 Entity::Entity() {
 
-}
+};
+
+void Update( float fDelta /* events */ /* camera */);
+
+};
